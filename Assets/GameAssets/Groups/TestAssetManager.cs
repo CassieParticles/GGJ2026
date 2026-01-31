fdd6e13c1634b80c41d4de6d4e11966bdb4fd955
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-31 01:09:12 +0000

    Added testing functions and placeholders to the scene

---
+++ Assets/GameAssets/Groups/TestAssetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+
+
+public class TestAssetManager : MonoBehaviour
+{
+    [SerializeField] private Groups group;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        GroupAssetSheet assetSheet = AssetManager.GetSheet(group);
+        if (assetSheet != null)
+        {
+            spriteRenderer.sprite = assetSheet.PersonSprite;
+        }
+    }
+}
