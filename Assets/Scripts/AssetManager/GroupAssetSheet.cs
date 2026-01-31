abf51ddbcd6e3dba561f2eebebbdef6c44082943
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-31 00:12:55 +0000

    Created GroupAssetSheet SO

---
+++ Assets/Scripts/AssetManager/GroupAssetSheet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "GroupAssetSheet", menuName = "AssetManager/GroupAssetSheet", order = 0)]
+public class GroupAssetSheet : ScriptableObject
+{
+    [SerializeField] private Groups group;
+    [SerializeField] private Sprite personSprite;
+
+    public Groups Group => group;
+    public Sprite PersonSprite => personSprite;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnValidate()
+    {
+        throw new NotImplementedException();
+    }
+}
