ee7f48e68d5658cc89009e8259492fe0010fcdd0
Adamson-Connor <adamsonconnor03@gmail.com>
2026-01-31 23:06:59 +0000

    Script Setup

--- Assets/Scripts/UiToolkit/SettingsController.cs
+++ Assets/Scripts/UiToolkit/SettingsController.cs
@@ -1,16 +1,43 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+
 
 public class SettingsController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private UIDocument SetDoc;
+    private VisualElement m_root;
+    private bool m_IsOpen;
+    private void Awake()
     {
-        
+        SetDoc = gameObject.GetComponent<UIDocument>();
+        m_root = SetDoc.rootVisualElement.Q<VisualElement>("Panel");
+
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMenuButton()
     {
-        
+        if(m_IsOpen)
+        {
+            CloseMenu();
+        }
+        else
+        {
+            OpenMenu();
+        }
+        m_IsOpen = !m_isOpen;
+    }
+
+    public void OpenMenu()
+    {
+        Time.timeScale = 0;
+        m_root.RemoveFromClassList("hidden");
+    }
+    public void CloseMenu()
+    {
+        Time.timeScale = 1;
+        m_root.AddToClassList("hidden");
     }
 }
