508fba06a323e1d9353da343789f7f65c6c87503
Adamson-Connor <adamsonconnor03@gmail.com>
2026-01-31 14:01:10 +0000

    functionality

--- Assets/Scripts/UiToolkit/UIController.cs
+++ Assets/Scripts/UiToolkit/UIController.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    public VisualElement ui;
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button quitButton;
     private void Awake()
     {
-        
+        ui = GetComponent<UIDocument>().rootVisualElement;
+    }
+    private void OnEnable()
+    {
+        playButton = ui.Q<Button>("Play");
+        playButton.clicked += OnPlayButtonClicked;
+        optionsButton = ui.Q<Button>("Settings");
+        optionsButton.clicked += OnOptionsButtonClicked;
+        quitButton = ui.Q<Button>("Exit");
+        quitButton.clicked += OnQuitButtonClicked;
+    }
+
+
+    private void OnQuitButtonClicked()
+    {
+        Application.Quit();
+
+    }
+    private void OnOptionsButtonClicked()
+    {
+        Debug.Log("Options");
+    }
+    private void OnPlayButtonClicked()
+    {
+        SceneManager.LoadSceneAsync(1);
     }
 }
