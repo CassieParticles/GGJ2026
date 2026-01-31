aebfbc8eb9500b1ead396137e376f33b5f913603
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-01-31 14:04:06 +0000

    Created AgitatorSpawner SO for use

--- Assets/Scripts/Agitators/AgitatorSpawner.cs
+++ Assets/Scripts/Agitators/AgitatorSpawner.cs
@@ -1,16 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "AgitatorSpawner", menuName = "Agitator/AgitatorSpawner", order = 0)]
 public class AgitatorSpawner : ScriptableObject
 {
     [SerializeField] private AgitatorBehaviour agitatorPrefab;
+    
+    [SerializeField] private GroupAssetSheet[]  groupAssetSheets;
 
-    AgitatorBehaviour CreateAgitator(Groups from, Groups to)
+    public AgitatorBehaviour CreateAgitator(Groups from, Groups to)
     {
         AgitatorBehaviour agitator = Instantiate(agitatorPrefab);
 
         //Set up agitator
         
+        //Get asset sheet
+        GroupAssetSheet assetSheet = null;
+        foreach (GroupAssetSheet sheet in groupAssetSheets)
+        {
+            if (sheet.Group == from)
+            {
+                assetSheet = sheet;
+                break;
+            }
+        }
+        
+        agitator.Initialize(from,to,assetSheet);
         
         return agitator;
     }
