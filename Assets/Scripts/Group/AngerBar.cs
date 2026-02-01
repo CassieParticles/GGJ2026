994c74d45000748124186e30257cb5fdca297534
Adamson-Connor <adamsonconnor03@gmail.com>
2026-02-01 12:50:07 +0000

    ProgBarsetup

--- Assets/Scripts/Group/AngerBar.cs
+++ Assets/Scripts/Group/AngerBar.cs
@@ -1,10 +1,20 @@
 ﻿
 using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AngerBar: MonoBehaviour
 {
     GroupBehaviour group;
+    public VisualElement gui;
+    public ProgressBar angerbar1;
+    public ProgressBar angerbar2;
+    public ProgressBar angerbar3;
+    public ProgressBar angerbar4;
+    public float angerval1;
+    public float angerval2;
+    public float angerval3;
+    public float angerval4;
     private void Awake()
     {
         group = GetComponentInParent<GroupBehaviour>();
@@ -16,15 +26,29 @@ public class AngerBar: MonoBehaviour
         }
         
         //TODO: Get GUI bar
+        gui = GetComponent<UIDocument>().rootVisualElement;
+        Debug.Log("I got the GUI OBJ");
+        angerbar1 = gui.Q<ProgressBar>("BeeBar");
+        angerbar2 = gui.Q<ProgressBar>("SharkBar");
+        angerbar3 = gui.Q<ProgressBar>("CrocBar");
+        angerbar4 = gui.Q<ProgressBar>("SkunkBar");
+        Debug.Log("BarsFound");
+
     }
 
     private void Start()
     {
         //Set initial values for GUI bar
+        angerval1 = 0;
+        angerval2 = 0;
+        angerval3 = 0;
+        angerval4 = 0;
+
     }
 
     private void FixedUpdate()
     {
         //TODO: Set GUI bar to value
+        // if bar hits zero Lose();
     }
 }
