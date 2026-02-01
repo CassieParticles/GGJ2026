dd9eec75c0b7c0c1e02c5a2360a8443f9297ade9
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-02-01 11:31:38 +0000

    AngerBar script

---
+++ Assets/Scripts/Group/AngerBar.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+public class AngerBar: MonoBehaviour
+{
+    GroupBehaviour group;
+    private void Awake()
+    {
+        group = GetComponentInParent<GroupBehaviour>();
+        if (!group)
+        {
+            Debug.LogError("ERROR: GROUP NOT LOCATED");
+            gameObject.SetActive(false);
+            return;
+        }
+        
+        //TODO: Get GUI bar
+    }
+
+    private void Start()
+    {
+        //Set initial values for GUI bar
+    }
+
+    private void FixedUpdate()
+    {
+        //TODO: Set GUI bar to value
+    }
+}
